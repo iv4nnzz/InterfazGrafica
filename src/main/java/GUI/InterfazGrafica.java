8672b720c0d4c49dc887ddeb443709404edfb9f3
ivan <9spotzs@gmail.com>
2025-10-15 19:01:57 -0500

    Sistema completado a su totalidad

---
+++ src/main/java/GUI/InterfazGrafica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package GUI;
+
+/**
+ *
+ * @author 9spot
+ */
+public class InterfazGrafica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
